27202dc5299a2e273f0e2f3725f85f7937e1331b
emilianostic <emilianostic@gmail.com>
2024-10-17 16:30:01 -0300

    hasta formulario creando nuevas tareas

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,15 +1,37 @@
+"use client";
+import { useRouter } from "next/navigation";
 function NewPage() {
+  const router = useRouter();
+  const handleSubnmit = async (e) => {
+    e.preventDefault();
+
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+    const res = await fetch("/api/tasks", {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "appication/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    router.push("/");
+  };
+
   return (
     <div className="h-screen flex justify-center items-center">
-      <form className="bg-slate-800 p-10">
+      <form
+        className="bg-slate-800 p-10 lg:w-1/4 md:w-1/2"
+        onSubmit={handleSubnmit}
+      >
         <label htmlFor="title" className="font-bold text-sm">
-          {" "}
           Título de la tarea
         </label>
         <input
           type="text"
           id="title"
-          className="border border-gray-400 p-2 mb-4 w-full rounded"
+          className="border border-gray-400 p-2 mb-4 w-full rounded text-black"
           placeholder="Título"
         />
         <label htmlFor="description" className="font-bold text-sm">
@@ -17,7 +39,8 @@ function NewPage() {
         </label>
         <textarea
           rows="3"
-          className="border border-gray-400 p-2 mb-4 w-full rounded"
+          id="description"
+          className="border border-gray-400 p-2 mb-4 w-full rounded text-black"
           placeholder="describe tu tarea"
         ></textarea>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4">
